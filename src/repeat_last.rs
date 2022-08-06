31f67c32c5511b53ded2b99e34401c2977749922
Rafael Bachmann <rafael.bachmann.93@gmail.com>
2022-08-06 21:19:54 +0200

    Improve last item repeater

--- src/repeat_last.rs
+++ src/repeat_last.rs
@@ -1,11 +1,12 @@
+use std::iter::Fuse;
+
 pub struct LastRepeatIter<I>
 where
     I: Iterator,
     I::Item: Clone,
 {
-    it: I,
+    it: Fuse<I>,
     next: Option<I::Item>,
-    last: bool,
 }
 
 impl<I> LastRepeatIter<I>
@@ -17,11 +18,8 @@ where
     where
         It: IntoIterator<IntoIter = I, Item = I::Item>,
     {
-        LastRepeatIter {
-            it: it.into_iter(),
-            next: None,
-            last: false,
-        }
+        let it = it.into_iter().fuse();
+        LastRepeatIter { it, next: None }
     }
 }
 
@@ -33,24 +31,29 @@ where
     type Item = I::Item;
 
     fn next(&mut self) -> Option<Self::Item> {
-        if !self.last {
-            match self.it.next() {
-                None => self.last = true,
-                Some(next) => self.next = Some(next),
-            }
+        if let Some(next) = self.it.next() {
+            self.next = Some(next)
         }
 
         self.next.clone()
     }
 }
 
-#[test]
-fn it_works() {
-    let mut it = LastRepeatIter::new(vec![1, 2, 3]);
+#[cfg(test)]
+mod test {
+    use super::*;
 
-    assert_eq!(it.next(), Some(1));
-    assert_eq!(it.next(), Some(2));
-    assert_eq!(it.next(), Some(3));
-    assert_eq!(it.next(), Some(3));
-    assert_eq!(it.next(), Some(3));
+    #[test]
+    fn it_works() {
+        let mut it = LastRepeatIter::new(vec![1, 2, 3]);
+
+        assert_eq!(it.next(), Some(1));
+        assert_eq!(it.next(), Some(2));
+        assert!(it.take(100).all(|n| n == 3));
+    }
+
+    #[test]
+    fn it_breaks() {
+        assert!(LastRepeatIter::new(None::<()>).next().is_none());
+    }
 }
