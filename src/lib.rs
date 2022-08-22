04454e28cbb45125b3c795ba498fa50ecca727e2
Rafael Bachmann <rafael.bachmann.93@gmail.com>
2022-08-22 22:56:45 +0200

    pub-licize items in monitor, source, watchdog

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,6 @@ mod source;
 mod status;
 mod watchdog;
 
-pub use crate::monitor::Monitor;
-pub use crate::source::Source;
-pub use crate::watchdog::Watchdog;
+pub use crate::monitor::*;
+pub use crate::source::*;
+pub use crate::watchdog::*;
